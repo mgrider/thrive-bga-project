5e4ab010daf08b20ecff16598cefa9500dc4061d
Jeremy Ward <jeremy@jmichaelward.com>
2021-03-07 18:42:10 -0600

    Tweak styles and add pegs to modal view by default.

--- thrive.view.php
+++ thrive.view.php
@@ -59,31 +59,31 @@
 		for( $n=0; $n<25; $n++ ) {
 			if ( $n != 12 ) {
 				switch( $n ) {
-					case "0": $x = -2; $y = -2 ; break;
-					case "1": $x = -1; $y = -2 ; break;
-					case "2": $x =  0; $y = -2 ; break;
-					case "3": $x =  1; $y = -2 ; break;
-					case "4": $x =  2; $y = -2 ; break;
-					case "5": $x = -2; $y = -1 ; break;
-					case "6": $x = -1; $y = -1 ; break;
-					case "7": $x =  0; $y = -1 ; break;
-					case "8": $x =  1; $y = -1 ; break;
-					case "9": $x =  2; $y = -1 ; break;
-					case "10": $x = -2; $y = 0 ; break;
-					case "11": $x = -1; $y = 0 ; break;
-					case "12": $x =  0; $y = 0 ; break;
-					case "13": $x =  1; $y = 0 ; break;
-					case "14": $x =  2; $y = 0 ; break;
-					case "15": $x = -2; $y = 1 ; break;
-					case "16": $x = -1; $y = 1 ; break;
-					case "17": $x =  0; $y = 1 ; break;
-					case "18": $x =  1; $y = 1 ; break;
-					case "19": $x =  2; $y = 1 ; break;
-					case "20": $x = -2; $y = 2 ; break;
-					case "21": $x = -1; $y = 2 ; break;
-					case "22": $x =  0; $y = 2 ; break;
-					case "23": $x =  1; $y = 2 ; break;
-					case "24": $x =  2; $y = 2 ; break;
+					case "0": $x = -2.1; $y = -2.1 ; break;
+					case "1": $x = -1.1; $y = -2.1 ; break;
+					case "2": $x =  -0.1; $y = -2.1 ; break;
+					case "3": $x =  0.9; $y = -2.1 ; break;
+					case "4": $x =  1.9; $y = -2.1 ; break;
+					case "5": $x = -2.1; $y = -1.1; break;
+					case "6": $x = -1.1; $y = -1.1; break;
+					case "7": $x =  -0.1; $y = -1.1; break;
+					case "8": $x =  0.9; $y = -1.1; break;
+					case "9": $x =  1.9; $y = -1.1; break;
+					case "10": $x = -2.1; $y = -0.1; break;
+					case "11": $x = -1.1; $y = -0.1; break;
+					case "12": $x =  -0.1; $y = -0.1; break;
+					case "13": $x =  0.9; $y = -0.1; break;
+					case "14": $x =  1.9; $y = -0.1; break;
+					case "15": $x = -2.1; $y = 0.9; break;
+					case "16": $x = -1.1; $y = 0.9; break;
+					case "17": $x =  -0.1; $y = 0.9; break;
+					case "18": $x =  0.9; $y = 0.9; break;
+					case "19": $x =  1.9; $y = 0.9; break;
+					case "20": $x = -2.1; $y = 1.9; break;
+					case "21": $x = -1.1; $y = 1.9; break;
+					case "22": $x =  -0.1; $y = 1.9; break;
+					case "23": $x =  0.9; $y = 1.9; break;
+					case "24": $x =  1.9; $y = 1.9; break;
 				}
 
 				$this->page->insert_block( "peg_placement", array(
